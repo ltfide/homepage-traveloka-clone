7cf34ff7e71f04b412e8ce8f9ba2c235855028c5
ltfide <lutfidendians@gmail.com>
2022-05-05 17:04:52 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        container: {
+            center: true,
+        },
+        extend: {
+            colors: {
+                primary: "#0194F3",
+                footer: "#1B1B1B",
+            },
+            fontFamily: {
+                inter: ["Inter", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
